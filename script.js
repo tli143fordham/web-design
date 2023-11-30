552e6680dd79fa753c2541f38be99ce5ed728d87
tli143fordham <143722234+tli143fordham@users.noreply.github.com>
2023-11-30 04:08:31 -0500

    yay

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $(".buttontd").click(function(){
+        $(".buttontd, #header, .curtains").hide();
+    });
+
+      //  $('.buttontd').click(function() {
+        
+       // $('.curtains').addClass('animate');
+        
+      //  });
+        
+});
